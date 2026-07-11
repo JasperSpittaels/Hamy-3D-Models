0cdadaefea36f63e9cf57ed20ad301c680c67c97
Jasper <jasper.spittaels@gmail.com>
2026-07-11 19:21:34 +0200

    Deploy Hamy 3D Models

--- script.js
+++ script.js
@@ -14,6 +14,44 @@ function openBuy(model, price) {
 	document.getElementById("popup").style.display="flex";
 }
 
+
+
 function closeBuy() {
 	document.getElementById("popup").style.display="none";
+}
+
+
+
+function sendPayment() {
+
+	let params = {
+		model:selectedModel,
+		price:selectedPrice,
+		time:new Date().toLocaleString()
+	};
+
+
+	emailjs.send(
+		"hamy_3d_models",
+		"hamy_3d_models_tempate",
+		params
+	)
+
+	.then(function() {
+
+		alert("Bedankt! Ik controleer de betaling en stuur de downloadlink.");
+
+		closeBuy();
+
+	})
+
+
+	.catch(function(error) {
+
+		alert("Er ging iets fout.");
+
+		console.log(error);
+
+	});
+
 }
